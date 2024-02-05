0f1cc9400f48ca4a36cc29669916b8ae11be68d1
ambers7 <ambrshen@gmail.com>
2024-02-05 17:14:52 -0500

    Create base pages for website functions

--- home.py
+++ home.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(_main_)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Welcome to NookVerse</h1>"
+    return render_template('home.html')
 
-if _name_ == "_main_":
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/book_bubble')
+def book_bubble():
+    return render_template('book_bubble.html')
+
+@app.route('/read_together')
+def read_together():
+    return render_template('read_together.html')
+
+@app.route('/see_data')
+def see_data():
+    return render_template('see_data.html')
+
+if __name__ == "_main_":
     app.run()
