2b8321b3c5c882dcb3e375c72d2a6b29ff0696f6
ambers7 <ambrshen@gmail.com>
2024-02-12 15:37:24 -0500

    Base pages for read together and see data

--- home.py
+++ home.py
@@ -1,6 +1,24 @@
-from flask import Flask, render_template
+# pip install flask
+# pip install flask-WTF
+
+
+# set FLASK_APP=home.py
+# $env:FLASK_APP = "home.py"
+# flask run
+
+from flask import Flask, render_template, flash, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from wtforms.validators import InputRequired
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['UPLOAD_FOLDER'] = 'static/files'
+
+class UploadFileForm(FlaskForm):
+    file = FileField("File", validators=[InputRequired()])
+    submit = SubmitField("Upload File")
 
 @app.route("/")
 def home():
@@ -13,9 +31,22 @@ def login():
 @app.route('/book_bubble')
 def book_bubble():
     return render_template('book_bubble.html')
+# @app.route('/home', methods=['GET',"POST"])
+# def home():
+#     form = UploadFileForm()
+#     if form.validate_on_submit():
+#         file = form.file.data # First grab the file
+#         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
+#         return render_template('results.html', results = asl_recognizer.recognize_image(file.filename), filename = file.filename)
+#     return render_template('index.html', form=form)
 
-@app.route('/read_together')
+@app.route('/read_together')#, methods=['GET',"POST"])
 def read_together():
+    # form = UploadFileForm()
+    # if form.validate_on_submit():
+    #     file = form.file.data #first grab the file 
+    #     file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
+    #     return render_template('book.html')#,results=file.filename) #pdf here 
     return render_template('read_together.html')
 
 @app.route('/see_data')
