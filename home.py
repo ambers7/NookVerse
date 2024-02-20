8171611ed37bb767c948956ac2beaeaf4721899c
ambers7 <ambrshen@gmail.com>
2024-02-20 15:32:40 -0500

    Upload pdf (redirect to book.html, file is saved to static/files)

--- home.py
+++ home.py
@@ -8,19 +8,22 @@
 
 from flask import Flask, render_template, flash, request, redirect, url_for
 from flask_wtf import FlaskForm
+from fileinput import filename 
 from wtforms import FileField, SubmitField
 from wtforms.validators import InputRequired
 from werkzeug.utils import secure_filename
+import os
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'static/files'
 
-class UploadFileForm(FlaskForm):
-    file = FileField("File", validators=[InputRequired()])
-    submit = SubmitField("Upload File")
+# class UploadFileForm(FlaskForm):
+#     file = FileField("File", validators=[InputRequired()])
+#     submit = SubmitField("Upload File")
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def home():
     return render_template('home.html')
 
@@ -31,22 +34,16 @@ def login():
 @app.route('/book_bubble')
 def book_bubble():
     return render_template('book_bubble.html')
-# @app.route('/home', methods=['GET',"POST"])
-# def home():
-#     form = UploadFileForm()
-#     if form.validate_on_submit():
-#         file = form.file.data # First grab the file
-#         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
-#         return render_template('results.html', results = asl_recognizer.recognize_image(file.filename), filename = file.filename)
-#     return render_template('index.html', form=form)
+    
+@app.route('/book', methods = ['POST'])   
+def book():   
+    if request.method == 'POST':   
+        f = request.files['file'] 
+        f.save(os.path.join(os.path.dirname(os.path.abspath(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(f.filename)))
+        return render_template("book.html", name = f.filename)   
 
-@app.route('/read_together')#, methods=['GET',"POST"])
+@app.route('/read_together', methods=['GET',"POST"])
 def read_together():
-    # form = UploadFileForm()
-    # if form.validate_on_submit():
-    #     file = form.file.data #first grab the file 
-    #     file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
-    #     return render_template('book.html')#,results=file.filename) #pdf here 
     return render_template('read_together.html')
 
 @app.route('/see_data')
