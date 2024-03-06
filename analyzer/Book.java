c7ca8f98aa571900c18ad64f011545b1fa58aea3
Mikayla Lin <mikayla.lin144@gmail.com>
2024-03-06 11:50:12 -0500

    created Book and wrote constructor

---
+++ analyzer/Book.java
@@ -0,0 +1,43 @@
+/*
+  Title
+  Authors
+  Contributors
+  ISBN/UID
+      Format
+  Read Status
+      Date Added
+      Last Date Read
+      Dates Read
+      Read Count
+      Moods
+      Pace
+      Character- or Plot-Driven?
+      Strong Character Development?
+      Loveable Characters?
+      Diverse Characters?
+      Flawed Characters?
+  Star Rating
+      Review
+      Content Warnings
+      Content Warning Description
+      Tags
+      Owned?
+*/
+
+public class Book{
+  private String title;
+  private String[] authors, contributors;
+  private long isbn;
+  private int status; //true if read, false if to read
+  private float rating;
+
+  //Constructor
+  public Book(String title, String[] authors, String[] contributors, long isbn, int status, float rating){
+    this.title = title;
+    this.authors = authors;
+    this.contributors = contributors;
+    this.isbn = isbn;
+    this.status = status;
+    this.rating = rating;
+  }
+}
