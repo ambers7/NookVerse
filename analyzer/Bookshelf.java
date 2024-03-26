18151aa706957d7f43bbfb3e74e0a2e92318ce03
Mikayla Lin <mikayla.lin144@gmail.com>
2024-03-26 09:51:16 -0400

    addBook

--- analyzer/Bookshelf.java
+++ analyzer/Bookshelf.java
@@ -9,4 +9,31 @@ public class Bookshelf{
     this.username = username;
     this.booklist = new ArrayList<Book>();
   }
+
+  public String username(){
+    return username;
+  }
+
+  public ArrayList<Book> booklist(){
+    return booklist;
+  }
+
+  //adds book to arraylist (alphabetically by author first, then title)
+  public boolean addBook(Book book){
+    //+ if this book is second, - if this book is first, 0 if they are identical
+    // if book.compare(next book) is +, continue, and if its -, add it in right before
+    for (int i = 0; i < booklist.size(); i++){
+      if (book.compare(booklist.get(i))<0){
+        booklist.add(i, book);
+        return true;
+      }
+    }
+    return false;
+  }
+
+/*
+  public Book removeBook(long isbn){
+    return NULL;
+  }
+  */
 }
